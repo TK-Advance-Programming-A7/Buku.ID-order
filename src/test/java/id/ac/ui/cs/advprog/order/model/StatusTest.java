c3f60e02e2f6eb4aac85b2244680a6a55c38cb15
vinkakniv <vinka.aeris@gmail.com>
2024-04-20 12:02:57 +0700

    [REFACTOR] Test directory structure

---
+++ src/test/java/id/ac/ui/cs/advprog/order/model/StatusTest.java
@@ -0,0 +1,43 @@
+package id.ac.ui.cs.advprog.order.model;
+import id.ac.ui.cs.advprog.order.model.Order;
+import id.ac.ui.cs.advprog.order.status.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.sql.Timestamp;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+public class StatusTest {
+
+    @Test
+    public void testAllStatusStateAndCancelable() throws Exception {
+        Order order = new Order(1);
+        assertEquals("Waiting Checkout", order.getStatus());
+        assertTrue(order.isCancelable());
+
+        order.nextStatus();
+        assertEquals("Waiting Payment", order.getStatus());
+        assertTrue(order.isCancelable());
+
+        order.nextStatus();
+        assertEquals("Waiting Delivered", order.getStatus());
+        assertFalse(order.isCancelable());
+
+
+        order = new Order(1);
+        order.cancelOrder();
+        assertEquals("Cancelled", order.getStatus());
+        order.nextStatus();
+        assertEquals("Cancelled", order.getStatus());
+        assertFalse(order.isCancelable());
+    }
+}

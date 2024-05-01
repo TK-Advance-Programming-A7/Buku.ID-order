586a432e6b168f682294da94ef96d8f444e005cc
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-05-01 18:00:44 +0700

    feat: sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,15 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "tk-a7-adpro_order")
+    property("sonar.organization", "tk-a7-adpro")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
 }
 
 group = "id.ac.ui.cs.advprog"
